a941c4117541e87cde8a91cc6c6b38ba5d7fc97b
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2020-01-14 12:00:45 +0100

    Include requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ with open('nekos/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
 if not version:
     raise RuntimeError('version is not set')
 
@@ -20,5 +25,6 @@ setup(
     packages=['nekos'],
     license='GNU v3',
     description='A Python module that uses Nekos API',
-    include_package_data=True
+    include_package_data=True,
+    install_requires=requirements
 )
