35443d520d4a8ec611aa544bfb88e3125b603f75
Maycon V <dev.mayconv@outlook.com>
2021-07-09 12:37:39 -0300

    requests replaced by httpx

--- nekos/http.py
+++ nekos/http.py
@@ -1,17 +1,17 @@
-import requests
+import httpx
 
 from .errors import NothingFound
 
 
 class RequestAPI:
-    def __init__(self, base_url) -> None:
+    def __init__(self, base_url: str) -> None:
         self.base_url = base_url
-        self._session = requests.Session()
 
     def get(self, endpoint: str) -> dict:
-        response = self._session.get(f"{self.base_url}{endpoint}")
-        if response.status_code in [404, 500]:
-            raise NothingFound("Unable to contact API.")
+        with httpx.Client(http2=True) as client:
+            response = client.get(f"{self.base_url}{endpoint}")
+            if response.status_code in [404, 500]:
+                raise NothingFound("Unable to contact API.")
         return response.json()
 
 
