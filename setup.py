ac5cd5cd2e8bae690bb7817c2949cf80863ff6bb
Motzumoto <cuztopher1e@gmail.com>
2022-05-01 09:05:25 -0400

    formatting

--- setup.py
+++ setup.py
@@ -3,28 +3,30 @@ import re
 from setuptools import setup
 
 
-version = ''
-with open('nekos/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+version = ""
+with open("nekos/__init__.py") as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    ).group(1)
 
 
 requirements = []
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 
 if not version:
-    raise RuntimeError('version is not set')
+    raise RuntimeError("version is not set")
 
 
 setup(
-    name='nekos.py',
-    author='AlexFlipnote',
-    url='https://github.com/Nekos-life/nekos.py',
+    name="nekos.py",
+    author="AlexFlipnote",
+    url="https://github.com/Nekos-life/nekos.py",
     version=version,
-    packages=['nekos'],
-    license='GNU v3',
-    description='A Python module that uses Nekos API',
+    packages=["nekos"],
+    license="GNU v3",
+    description="A Python module that uses Nekos API",
     include_package_data=True,
-    install_requires=requirements
+    install_requires=requirements,
 )
