a92b2a2d38f9423db817a86f637cb042d3ec6428
Motzumoto <cuztopher1e@gmail.com>
2022-05-01 09:05:42 -0400

    new funcs, removed dead endpoints, formatting

--- nekos/nekos.py
+++ nekos/nekos.py
@@ -7,30 +7,46 @@ noresponse = "Couldn't contact the API right now..."
 
 def eightball():
     r = http.get("/8ball")
-    return dict.JsonDict({
-        "text": r["response"],
-        "image": r["url"]
-    })
+    return dict.JsonDict({"text": r["response"], "image": r["url"]})
 
 
 def img(target: str):
     possible = [
-        'feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
-        'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk',
-        'ngif', 'tickle', 'lewd', 'feed', 'gecg', 'eroyuri', 'eron',
-        'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi', 'nsfw_avatar',
-        'gasm', 'poke', 'anal', 'slap', 'hentai', 'avatar', 'erofeet', 'holo',
-        'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'lizard', 'pussy_jpg',
-        'pwankg', 'classic', 'kuni', 'waifu', 'pat', '8ball', 'kiss', 'femdom',
-        'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs', 'random_hentai_gif',
-        'smallboobs', 'hug', 'ero', 'smug', 'goose', 'baka', 'woof'
+        "wallpaper",
+        "ngif",
+        "tickle",
+        #"lewd", Provides 1 image
+        "feed",
+        "gecg",
+        "gasm",
+        "slap",
+        "avatar",
+        "lizard",
+        "waifu",
+        "pat",
+        "8ball",
+        "kiss",
+        "neko",
+        "spank",
+        "cuddle",
+        "fox_girl",
+        "hug",
+        "smug",
+        "goose",
+        "woof",
     ]
 
     if target is None:
-        raise errors.EmptyArgument("You have to at least define an argument in string format\nArguments: {}".format(possible))
+        raise errors.EmptyArgument(
+            "You have to at least define an argument in string format\nArguments: {}".format(
+                possible
+            )
+        )
 
     if target.lower() not in possible:
-        raise errors.InvalidArgument("You haven't added any valid arguments\nArguments: {}".format(possible))
+        raise errors.InvalidArgument(
+            "You haven't added any valid arguments\nArguments: {}".format(possible)
+        )
 
     try:
         if target.lower() == "random_hentai_gif":
@@ -45,9 +61,21 @@ def img(target: str):
 
 def owoify(text: str):
     if text is None:
-        raise errors.EmptyArgument("You have to enter a string you want to enter to API")
+        raise errors.EmptyArgument(
+            "You have to enter a string you want to enter to API"
+        )
 
     r = http.get("/owoify?text=" + urllib.parse.quote(text))
+    return r["owo"]
+
+
+def spoiler(text: str):
+    if text is None:
+        raise errors.EmptyArgument(
+            "You have to enter a string you want to enter to API"
+        )
+
+    r = http.get("/spoiler?text=" + urllib.parse.quote(text))
     return r["owo"]
 
 
@@ -77,3 +105,11 @@ def fact():
         return http.get("/fact")["fact"]
     except Exception as e:
         raise errors.NothingFound(noresponse)
+
+
+def name():
+    try:
+        return http.get("/name")["name"]
+    except Exception as e:
+        raise errors.NothingFound(noresponse)
+
